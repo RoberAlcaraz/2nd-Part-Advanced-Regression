b9597d0f13e22ec1ef4f52fa23cb44ab2b5444d0
Roberto Alcaraz <roberto@irba.es>
2021-05-14 10:24:21 +0200

    first commit

---
+++ 03_shinyapp/Server.R
@@ -0,0 +1,13 @@
+################################################################################
+# title: "1st Part: Advanced Regression and Prediction"
+# author: "Roberto J. Alcaraz Molina"
+# date: "09/05/2021"
+# https://mastering-shiny.org/
+# SERVER FUNCTION
+################################################################################
+
+# install.packages("pacman")
+pacman::p_load(shiny, tidyverse, tidymodels, COVID19, shinythemes, thematic,
+               bslib, plotly, tidyquant, mixOmics)
+
+
